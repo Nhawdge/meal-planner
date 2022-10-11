dded2b036e2fda5d435c27e608699eccde5d1992
John Pavek <JohnQPavek@gmail.com>
2022-10-10 22:03:42 -0500

    Maybe it'll work?

--- src/web/js/main.js
+++ src/web/js/main.js
@@ -5,7 +5,65 @@ createApp({
     return {
       dragPayload: null,
       items: [],
-      mealTimes :["Breakfast", "Lunch", "Dinner"],
+      weeklySchedule: [],
+      defaultSchedule: [
+        {
+          day: "Sunday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Monday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Tuesday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Wednesday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Thursday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Friday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+        {
+          day: "Saturday",
+          meals: [
+            { mealTime: "Breakfast", items: [] },
+            { mealTime: "Lunch", items: [] },
+            { mealTime: "Dinner", items: [] },
+          ],
+        },
+      ],
     };
   },
   computed: {},
@@ -13,16 +71,45 @@ createApp({
     dragStart: function (event, payload) {
       this.dragPayload = JSON.stringify(payload);
     },
-    dragDrop: function (event) {
-      event.target.innerHTML += `<div>${JSON.parse(this.dragPayload).name}</div>`
+    dragDrop: function (event, meal) {
+      //event.target.innerHTML += `<div>${JSON.parse(this.dragPayload).name} <button v-on:click="clearCell">X</button></div>`;
+      meal.items.push(JSON.parse(this.dragPayload));
       this.dragPayload = null;
+      this.saveSchedule();
+    },
+    removeFood: function (meal, index) {
+      meal.items.splice(index, 1);
+      this.saveSchedule();
+    },
+    saveSchedule: function () {
+      localStorage.setItem("menu", JSON.stringify(this.weeklySchedule));
+    },
+    clearMenu: function () {
+      localStorage.removeItem("menu");
+      this.weeklySchedule = this.defaultSchedule;
     },
   },
   mounted: function () {
+    var data = JSON.parse(localStorage.getItem("menu"));
+    if (data) {
+      this.weeklySchedule = data;
+    } else {
+      this.weeklySchedule = this.defaultSchedule;
+    }
     fetch("/api/meals?limit=100")
       .then((x) => x.json())
       .then((data) => {
-        this.items = data.docs;
+        this.items = data.docs.map((x) => new MealCell(x));
       });
   },
 }).mount("#app");
+
+class MealCell {
+  constructor(meal) {
+    this.meal = meal;
+    this.createdAt = meal.createdAt;
+    this.id = meal.id;
+    this.ingredients = meal.ingredients;
+    this.name = meal.name;
+  }
+}
